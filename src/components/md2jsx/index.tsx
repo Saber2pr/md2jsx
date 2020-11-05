7a2ec7a84d00bdb24841afda32bd557035956ce9
saber2pr <1029985799@qq.com>
2020-11-05 09:42:03 +0800

    fix crlf

--- src/components/md2jsx/index.tsx
+++ src/components/md2jsx/index.tsx
@@ -4,14 +4,14 @@
  * @Last Modified by: saber2pr
  * @Last Modified time: 2020-04-07 14:10:59
  */
-import React, { Fragment, useEffect, useRef } from "react"
-import ClipboardJS from "clipboard"
+import React, { Fragment, useEffect, useRef } from 'react'
+import ClipboardJS from 'clipboard'
 
-import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/prism-light"
+import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/prism-light'
 
-import { REG } from "../../reg"
-import { mergeCode } from "../../core"
-import "./style.less"
+import { REG } from '../../reg'
+import { mergeCode } from '../../core'
+import './style.less'
 
 export interface Md2jsx {
   children: string
@@ -20,28 +20,28 @@ export interface Md2jsx {
 
 export function Md2jsx({ children, theme }: Md2jsx) {
   const jsx: JSX.Element[] = []
-  const lines = mergeCode(children.split("\n"))
+  const lines = mergeCode(children.split(/\n|\r\n/))
 
   for (let i = 0; i < lines.length; ++i) {
     const line = lines[i]
-    if (line.startsWith("```")) {
+    if (line.startsWith('```')) {
       renderCode({ jsx, i, line, theme })
-    } else if (line.startsWith("#")) {
+    } else if (line.startsWith('#')) {
       renderHeader({ jsx, i, line })
-    } else if (line.startsWith(">")) {
+    } else if (line.startsWith('>')) {
       renderComment({ jsx, i, line })
     } else if (REG.imgtype.test(line)) {
       renderImg({ jsx, i, line })
     } else if (REG.atype_url.test(line)) {
       renderAnchor({ jsx, i, line })
-    } else if (line.startsWith("---") || line.startsWith("***")) {
+    } else if (line.startsWith('---') || line.startsWith('***')) {
       renderHR({ jsx, i, line })
     } else {
       jsx.push(
         <p
           key={i}
           dangerouslySetInnerHTML={{
-            __html: line.replace(/\t/g, "&nbsp;&nbsp;")
+            __html: line.replace(/\t/g, '&nbsp;&nbsp;'),
           }}
         />
       )
@@ -72,14 +72,14 @@ const renderCode = ({ line, jsx, theme, i }: RenderLine & { theme }) => {
       const cp = new ClipboardJS(ref.current)
       return () => cp.destroy()
     }, [])
-    const id = "MD2JSX-Code-" + i
+    const id = 'MD2JSX-Code-' + i
     return (
       <div className="MD2JSX-Code">
         <div
           title="复制到剪贴板"
           className="Paste"
           ref={ref}
-          data-clipboard-target={"#" + id}
+          data-clipboard-target={'#' + id}
         >
           <i className="iconfont icon-fuzhi" />
         </div>
@@ -97,9 +97,9 @@ const renderCode = ({ line, jsx, theme, i }: RenderLine & { theme }) => {
 
 const renderHeader = ({ line, jsx, i }: RenderLine) => {
   let count = 0
-  let text = ""
+  let text = ''
   for (const ch of line) {
-    if (ch === "#") {
+    if (ch === '#') {
       count++
     } else {
       text += ch
@@ -115,9 +115,9 @@ const renderComment = ({ jsx, i, line }: RenderLine) => {
     <p
       key={i}
       style={{
-        color: "gray",
-        borderLeft: "0.2rem solid #d0d0d0",
-        paddingLeft: "0.5rem"
+        color: 'gray',
+        borderLeft: '0.2rem solid #d0d0d0',
+        paddingLeft: '0.5rem',
       }}
     >
       {line.slice(2)}
@@ -127,15 +127,15 @@ const renderComment = ({ jsx, i, line }: RenderLine) => {
 
 const renderImg = ({ jsx, i, line }: RenderLine) => {
   const meta = line.match(REG.imgtype)[0]
-  const title = meta.match(REG.imgtype_title)[0].replace(/\[|\]/g, "")
-  const url = meta.match(REG.imgtype_url)[0].replace(/\(|\)/g, "")
-  jsx.push(<img key={i} src={url} alt={title} style={{ maxWidth: "100%" }} />)
+  const title = meta.match(REG.imgtype_title)[0].replace(/\[|\]/g, '')
+  const url = meta.match(REG.imgtype_url)[0].replace(/\(|\)/g, '')
+  jsx.push(<img key={i} src={url} alt={title} style={{ maxWidth: '100%' }} />)
 }
 
 const renderAnchor = ({ jsx, i, line }: RenderLine) => {
   const meta = line.match(REG.atype_url)[0]
-  const title = meta.match(REG.imgtype_title)[0].replace(/\[|\]/g, "")
-  const url = meta.match(REG.imgtype_url)[0].replace(/\(|\)/g, "")
+  const title = meta.match(REG.imgtype_title)[0].replace(/\[|\]/g, '')
+  const url = meta.match(REG.imgtype_url)[0].replace(/\(|\)/g, '')
   jsx.push(
     <div key={url + title + i}>
       <a href={url} title={url}>
